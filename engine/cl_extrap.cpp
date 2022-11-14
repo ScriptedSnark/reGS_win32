fb6eb5454d57b939ef3f0621ae078313ab5ca364
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2022-11-14 17:44:05 +0500

    engine: empty cl_extrap.cpp

---
+++ engine/cl_extrap.cpp
@@ -0,0 +1,43 @@
+#include "quakedef.h"
+#include "cl_entity.h"
+
+void CL_InitExtrap()
+{
+	// TODO: implement - ScriptedSnark
+}
+
+qboolean CL_FindInterpolationUpdates(cl_entity_t* ent, float targettime, position_history_t** ph0, position_history_t** ph1, int* ph0Index)
+{
+	// TODO: implement - ScriptedSnark
+	return false;
+}
+
+void CL_BoundInterpolationFraction(float* frac)
+{
+	// TODO: implement - ScriptedSnark
+}
+
+void CL_UpdatePositions(cl_entity_t* ent)
+{
+	// TODO: implement - ScriptedSnark
+}
+
+void CL_ResetPositions(cl_entity_t* ent)
+{
+	// TODO: implement - ScriptedSnark
+}
+
+void CL_PureOrigin(cl_entity_t* ent, float t, vec_t* outorigin, vec_t* outangles)
+{
+	// TODO: implement - ScriptedSnark
+}
+
+void CL_ComputePlayerOrigin(cl_entity_t* ent)
+{
+	// TODO: implement - ScriptedSnark
+}
+
+void CL_InterpolateEntity(cl_entity_t* ent)
+{
+	// TODO: implement - ScriptedSnark
+}
