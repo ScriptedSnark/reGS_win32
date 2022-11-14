fdbf17b26125e0594a2a78e14d3bd14ecc11aed6
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2022-11-14 18:04:39 +0500

    engine: implement CL_InitExtrap, ex_interp cvar

--- engine/cl_extrap.cpp
+++ engine/cl_extrap.cpp
@@ -1,9 +1,11 @@
 #include "quakedef.h"
 #include "cl_entity.h"
 
+cvar_t ex_interp = { "ex_interp", "0.1", FCVAR_ARCHIVE | FCVAR_FILTERABLE };
+
 void CL_InitExtrap()
 {
-	// TODO: implement - ScriptedSnark
+	Cvar_RegisterVariable(&ex_interp);
 }
 
 qboolean CL_FindInterpolationUpdates(cl_entity_t* ent, float targettime, position_history_t** ph0, position_history_t** ph1, int* ph0Index)
