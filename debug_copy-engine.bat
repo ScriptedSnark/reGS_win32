393e58bfa47d8bf67f4c737306b71a3e8a9ab22d
ScriptedSnark <51358194+ScriptedSnark@users.noreply.github.com>
2022-11-15 20:27:41 +0500

    Add debug_copy-engine.bat

---
+++ debug_copy-engine.bat
@@ -0,0 +1,2 @@
+copy ".\engine\GL_Debug_STEAM\hw.dll" "C:\Program Files (x86)\Steam\steamapps\common\HL1_GS"
+copy ".\engine\GL_Debug_STEAM\hw.pdb" "C:\Program Files (x86)\Steam\steamapps\common\HL1_GS"
